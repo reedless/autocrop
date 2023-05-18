75a60e83fa882d9f894e9e21f46c74d3a99b9d24
Howard Yang <howardyanghaowen@gmail.com>
2023-05-18 16:37:47 +0800

    use railway template

--- main.py
+++ main.py
@@ -41,5 +41,4 @@ def autocrop():
         return jsonify(response), 400
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
-
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
