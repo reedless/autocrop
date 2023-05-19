e224076105ac6d30cb5d035029bf1ba4baadb7f1
Howard Yang <howardyanghaowen@gmail.com>
2023-05-20 01:15:24 +0800

    catch error in test

--- test.py
+++ test.py
@@ -7,30 +7,34 @@ import requests
 
 def post_image(url, image_path):
     files = {'image': open(image_path, 'rb')}
-    params = {'prompt': 'cat'}
-    
+    params = {'prompt': 'dog'}
+
     # Send POST request
     response = requests.post(url, files=files, data=params)
 
     return response
 
 # URL to post the image
-url = "http://127.0.0.1:5000/autocrop"
-# url = "https://autocrop-production.up.railway.app/autocrop"
+# url = "http://127.0.0.1:5000/autocrop"
+url = "https://autocrop-production.up.railway.app/autocrop"
 
 # Path to the image file
-image_path = "./test_cat.jpg"
+image_path = "./test_dog.jpg"
 
 # Call the function to post the image
 response = post_image(url, image_path)
 
-# Get the returned image
-cropped_image_data = response.json()['cropped_image_data']
-jpg_original = base64.b64decode(cropped_image_data)
-jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
-img = cv2.imdecode(jpg_as_np, flags=1)
+if response.status_code == 200:
+    # Get the returned image
+    cropped_image_data = response.json()['cropped_image_data']
+    jpg_original = base64.b64decode(cropped_image_data)
+    jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+    img = cv2.imdecode(jpg_as_np, flags=1)
 
-# Display the image using cv2
-save_path = "./received_from_post.jpg"
-cv2.imwrite(save_path, img)
-print(f" The image with the result is saved in: {save_path}")
+    # Display the image using cv2
+    save_path = "./received_from_post.jpg"
+    cv2.imwrite(save_path, img)
+    print(f" The image with the result is saved in: {save_path}")
+else:
+    print(response.status_code)
+    print(response.text)
