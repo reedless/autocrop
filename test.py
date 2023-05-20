cb5046fd3ff708c22ef055a7e5e7baf51ce11039
Howard Yang <howardyanghaowen@gmail.com>
2023-05-20 14:11:52 +0800

    added jpg files to gitignore

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import cv2
 import numpy as np
 import requests
 
+prompt = 'cat'
 
 def post_image(url, image_path):
     files = {'image': open(image_path, 'rb')}
-    params = {'prompt': 'dog'}
+    params = {'prompt': prompt}
 
     # Send POST request
     response = requests.post(url, files=files, data=params)
@@ -19,7 +20,7 @@ def post_image(url, image_path):
 url = "https://autocrop-production.up.railway.app/autocrop"
 
 # Path to the image file
-image_path = "./test_dog.jpg"
+image_path = f"./test_{prompt}.jpg"
 
 # Call the function to post the image
 response = post_image(url, image_path)
